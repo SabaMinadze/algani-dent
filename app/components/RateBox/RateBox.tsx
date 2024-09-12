7e35516f42476169ef87713133d49f78fa3af426
Saba <92267802+SabaMinadze@users.noreply.github.com>
2024-09-12 17:11:35 +0400

    Add code

--- app/components/RateBox/RateBox.tsx
+++ app/components/RateBox/RateBox.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import s from "./RateBox.module.css"
+import Image from 'next/image'
+import saba from "./saba.jpg"
 
 function RateBox() {
   return (
     <>
-    <div className={s.rateBox}>Hello</div>
+    <div className={s.rateBox}>
+      <Image src={saba} height={100} width={100} className={s.rateBoxImg}/>
+    </div>
     </>
   )
 }
