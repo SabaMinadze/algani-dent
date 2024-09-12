4fc5a06875f14bf2229ceca8cf61e1c2fc5108b8
Saba <92267802+SabaMinadze@users.noreply.github.com>
2024-09-12 20:28:55 +0400

    .

--- app/components/Main/Main.tsx
+++ app/components/Main/Main.tsx
@@ -7,7 +7,7 @@ import s from "./Main.module.css"
 function Main() {
   return (
     <>
-    <center><div>
+    <center><div className={s.mainBox1}>
         <h1 className={s.mainTxt}>იხილედ ჩვენი თანამშრომლები რომლებსაც აქვთ მოქნილი ხელი <Link href="/stomatologi"><Button title="დააჭირე აქ"/></Link></h1>
         
     </div></center>
