b47bf974240224284caa65a6fd63228e890d3658
Saba <92267802+SabaMinadze@users.noreply.github.com>
2024-09-12 16:48:40 +0400

    Add code

--- app/components/HelloTxt/HelloTxt.tsx
+++ app/components/HelloTxt/HelloTxt.tsx
@@ -2,12 +2,13 @@ import React from 'react'
 import s from "./HelloTxt.module.css"
 import Link from 'next/link'
 import Image from 'next/image'
+import logo from "./algani.png"
 
 function HelloTxt() {
   return (
     <center><div className={s.mainBox2}>
-        <h1>ალგანი დენტი</h1>
-        <center><Image src={logo} alt='error' height={200}  className={s.img2}/></center>
+        <h1 className={s.txt}>ალგანი დენტი</h1>
+        <center><Image src={logo} alt='error' height={300}  className={s.img2}/></center>
         <Link href="/asdda"><button className={s.btn4}>კითხვა გაქვთ? დაგვიკავშირდით</button></Link>
 
     </div></center>
