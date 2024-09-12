66a6a62ff1ebea5569d08d155768dbc7d5c250b9
Saba <92267802+SabaMinadze@users.noreply.github.com>
2024-09-12 19:38:50 +0400

    .

---
+++ app/components/ServiceAndPrice/ServiceAndPrice.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import s from "./ServiceAndPrice.module.css"
+
+function ServiceAndPrice() {
+  return (
+    <>
+    <div>
+        <h1>იხელედ ჩვენი სერვისები და ფასები აქ</h1>
+        <h1>
+            |
+            |
+            |
+            |
+            |
+            |
+            |
+            |
+            
+        </h1>
+    </div>
+    </>
+  )
+}
+
+export default ServiceAndPrice
