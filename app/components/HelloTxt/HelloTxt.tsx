ffe1b362d69607b1941a24e089d3aaedef167f4c
Saba <92267802+SabaMinadze@users.noreply.github.com>
2024-09-12 16:53:59 +0400

    Add code

--- app/components/HelloTxt/HelloTxt.tsx
+++ app/components/HelloTxt/HelloTxt.tsx
@@ -9,7 +9,7 @@ function HelloTxt() {
     <center><div className={s.mainBox2}>
         <h1 className={s.txt}>ალგანი დენტი</h1>
         <center><Image src={logo} alt='error' height={300}  className={s.img2}/></center>
-        <Link href="/asdda"><button className={s.btn4}>კითხვა გაქვთ? დაგვიკავშირდით</button></Link>
+        <Link href="/questions"><button className={s.btn4}>კითხვა გაქვთ? დაგვიკავშირდით</button></Link>
 
     </div></center>
   )
---
+++ app/components/Rate/Rate.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import s from "./Rate.module.css"
+
+function Rate() {
+  return (
+    <div>Rate</div>
+  )
+}
+
+export default Rate
