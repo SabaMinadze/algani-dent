e3b8197b0ab9a2f6c53bf8a7e0e2cf74e31cb9e4
Saba <92267802+SabaMinadze@users.noreply.github.com>
2024-09-12 20:12:11 +0400

    .

--- app/components/ServiceAndPrice/ServiceAndPrice.tsx
+++ app/components/ServiceAndPrice/ServiceAndPrice.tsx
@@ -1,18 +1,20 @@
 import React from 'react'
 import s from "./ServiceAndPrice.module.css"
 import Image from 'next/image'
-import arrowImage from "./arr2.jpg"
+import arrowImage from "./images.png"
+import Button from '../Button/Button'
+import Link from 'next/link'
 
 function ServiceAndPrice() {
   return (
     <>
-    <div>
-        <h1>იხელედ ჩვენი სერვისები და ფასები აქ</h1>
-        <div className={s.line}>a</div>
-        <button className={s.arrowButton}>
-            <div className={s.arrow}></div>
-        </button>
-    </div>
+    <center><div>
+        <h1 className={s.simpleTxtr}>გამარჯობათ ჩვენ გვაქვს ყველაზე ხელმისაწვდომი სერვისები ფასები</h1>
+        <h1 className={s.simpleTxt}>იხელედ ჩვენი სერვისები და ფასები აქ</h1>
+        <Image src={arrowImage} alt={''} className={s.arrImage}/>
+        <br />
+        <Link href="/servicesandprices"><Button title="დააჭირე და იხილე"/></Link>
+    </div></center>
     </>
   )
 }
