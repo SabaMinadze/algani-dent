5240cb59feba41a69d8574bbe391459573e299d7
Saba <92267802+SabaMinadze@users.noreply.github.com>
2024-09-12 17:51:07 +0400

    Update Rate.tsx

--- app/components/Rate/Rate.tsx
+++ app/components/Rate/Rate.tsx
@@ -1,26 +1,8 @@
 import React from 'react'
 import s from "./Rate.module.css"
 import RateBox from '../RateBox/RateBox'
-import { motion } from 'framer-motion';
 
 
-
-
-const containerVariants = {
-  hidden: { opacity: 0, y: 20 },
-  visible: {
-    opacity: 1,
-    y: 0,
-    transition: {
-      staggerChildren: 0.3, // Delay between each testimonial animation
-    }
-  }
-};
-
-const itemVariants = {
-  hidden: { opacity: 0, y: 50 },
-  visible: { opacity: 1, y: 0 }
-};
 
 function Rate() {
   return (
@@ -28,14 +10,9 @@ function Rate() {
     <div className={s.rateMainBox}>
         <h1 className={s.rateText}>რას ამბობენ პაციენტები ჩვენზე</h1>
         <div className={s.rateBoxes}>
-        <motion.div
-          className="testimonial"
-          variants={itemVariants}
-        >
-            <RateBox/>
-            <RateBox/>
-            <RateBox/>
-          </motion.div>
+          <RateBox/>
+          <RateBox/>
+          <RateBox/>
         </div>
     </div>
     </>
