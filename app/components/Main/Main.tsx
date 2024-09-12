36c1737e488ab2e740bfc8434394ce69a209ac5e
Saba <92267802+SabaMinadze@users.noreply.github.com>
2024-09-12 20:25:09 +0400

    .

--- app/components/Main/Main.tsx
+++ app/components/Main/Main.tsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import Button from '../Button/Button'
 import Link from 'next/link'
+import s from "./Main.module.css"
 
 
 function Main() {
   return (
     <>
     <center><div>
-        <h1>იხილედ ჩვენი თანამშრომლები რომლებსაც აქვთ მოქნილი ხელი <Link href="/stomatologi"><Button title="დააჭირე აქ"/></Link></h1>
+        <h1 className={s.mainTxt}>იხილედ ჩვენი თანამშრომლები რომლებსაც აქვთ მოქნილი ხელი <Link href="/stomatologi"><Button title="დააჭირე აქ"/></Link></h1>
         
     </div></center>
     </>
