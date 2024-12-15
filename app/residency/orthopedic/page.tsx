80dad9bf3c2f0cc19fb462c2cb5685a69730b543
User <sabatvaazz100@gmail.com>
2024-12-15 22:09:29 +0400

    Add new component

---
+++ app/residency/orthopedic/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import styles from './page.module.css'
+
+function page() {
+  return (
+    <div className={styles.box}>
+          <h1>ორთოპედიული რეზიდენტურა . თეორიული და პრაქტიკული კურსის ხანგძლივობა  15 თვე. პროგრამის ხელმძღვანელი - მარიამ
+          კვირიკაშვილი
+          </h1>
+    </div>
+  )
+}
+
+export default page
