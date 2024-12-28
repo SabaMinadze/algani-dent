b7e0c0930d69a92d7a07938e0963e153f10c505c
User <sabatvaazz100@gmail.com>
2024-12-29 01:11:13 +0400

    Update Main.tsx

--- app/components/Main/Main.tsx
+++ app/components/Main/Main.tsx
@@ -8,8 +8,7 @@ function Main() {
   return (
     <>
     <center><div className={s.mainBox1}>
-        <h1 className={s.mainTxt}>იხილედ ჩვენი თანამშრომლები<Link href="/stomatologi"><Button title="დააჭირე აქ"/></Link></h1>
-        
+        <h1 className={s.mainTxt}>იხილედ ჩვენი თანამშრომლები <Link href="/stomatologi"><Button title="დააჭირე აქ"/></Link></h1>
     </div></center>
     </>
   )
