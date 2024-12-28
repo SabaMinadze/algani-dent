4e31b602e628a3ff16e0ac436b861da72effb9f6
User <sabatvaazz100@gmail.com>
2024-12-29 01:07:18 +0400

    .

--- app/components/Footer/Footer.tsx
+++ app/components/Footer/Footer.tsx
@@ -1,17 +1,11 @@
 import React from 'react'
 import styles from './Footer.module.css'
-import Link from 'next/link'
 
 
 function Footer() {
   return (
     <div className={styles.footer}>
-      <h1 className={styles.txt}>MADE BY SABA MINADZE</h1>
-      <h2 className={styles.txt3}>Instagram: minadzesaba16</h2>
-      <br/><br/>
-      <h1>Language</h1>
-      <Link href="https://en-pink.vercel.app/"><button className={styles.btn}>English</button></Link>
-
+      <h2 className={styles.txt3}>მისამართი: თბილისი დიდუბე ვახუშტი ბაგრატიონის 19, Tbilisi, Georgia</h2>
       <h2 className={styles.txt2}>algandent.ge ყველა უფლება დაცულია. © 2023 - 2024</h2>
 
 
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,6 @@
 import React from 'react'
-import Subnav from './components/Subnav/Subnav'
-import Video from './components/Video/Video'
-import styles from './page.module.css'
 import Burgernav from './components/Burgernav/Burgernav'
-import Image from 'next/image'
 import HelloTxt from './components/HelloTxt/HelloTxt'
-import Rate from './components/Rate/Rate'
 import ServiceAndPrice from './components/ServiceAndPrice/ServiceAndPrice'
 import Main from './components/Main/Main'
 
@@ -17,7 +12,6 @@ function page() {
     <HelloTxt/>
     <ServiceAndPrice/>
     <Main/>
-    <Rate/>
     </>
   )
 }
